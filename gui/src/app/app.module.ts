77570d7e07964df8e703a9a4787175de2782ce0c
syotano1 <your_email@example.com>
2021-12-26 01:09:51 +0900

    add: とりあえず必要そうなコンポーネントを作成

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -5,16 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { StartToolBarComponent } from './pages/start-page/start-tool-bar/start-tool-bar.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartToolBarComponent,
+    StartPageComponent,
+    SearchPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
